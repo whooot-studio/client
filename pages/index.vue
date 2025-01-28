5cbbe9ad319a45121fea1b53286d4401d60b8f12
josselinonduty <contact@josselinonduty.fr>
2025-01-28 16:58:42 +0100

    feat: implement horizontal navigation component with links

--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,37 @@
+<script setup lang="ts">
+const links = [
+  [],
+  [],
+  // [
+  //   {
+  //     label: "Installation",
+  //     icon: "i-heroicons-home",
+  //     to: "/getting-started/installation",
+  //   },
+  //   {
+  //     label: "Horizontal Navigation",
+  //     icon: "i-heroicons-chart-bar",
+  //     to: "/components/horizontal-navigation",
+  //   },
+  //   {
+  //     label: "Command Palette",
+  //     icon: "i-heroicons-command-line",
+  //     to: "/components/command-palette",
+  //   },
+  // ],
+  // [
+  //   {
+  //     label: "Examples",
+  //     icon: "i-heroicons-light-bulb",
+  //   },
+  //   {
+  //     label: "Help",
+  //     icon: "i-heroicons-question-mark-circle",
+  //   },
+  // ],
+];
+</script>
+
 <template>
-  <p>Hello, world!</p>
+  <UHorizontalNavigation :links="links" />
 </template>
