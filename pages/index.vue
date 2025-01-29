6fb58336db7c01a64d27ae115940b9be78ae029b
josselinonduty <contact@josselinonduty.fr>
2025-01-29 18:02:24 +0100

    feat: add quiz link to navigation

--- pages/index.vue
+++ pages/index.vue
@@ -1,34 +1,13 @@
 <script setup lang="ts">
 const links = [
+  [
+    {
+      label: "quiz",
+      icon: "i-heroicons-home",
+      to: "/quiz",
+    },
+  ],
   [],
-  [],
-  // [
-  //   {
-  //     label: "Installation",
-  //     icon: "i-heroicons-home",
-  //     to: "/getting-started/installation",
-  //   },
-  //   {
-  //     label: "Horizontal Navigation",
-  //     icon: "i-heroicons-chart-bar",
-  //     to: "/components/horizontal-navigation",
-  //   },
-  //   {
-  //     label: "Command Palette",
-  //     icon: "i-heroicons-command-line",
-  //     to: "/components/command-palette",
-  //   },
-  // ],
-  // [
-  //   {
-  //     label: "Examples",
-  //     icon: "i-heroicons-light-bulb",
-  //   },
-  //   {
-  //     label: "Help",
-  //     icon: "i-heroicons-question-mark-circle",
-  //   },
-  // ],
 ];
 </script>
 
