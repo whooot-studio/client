a51349bf07076f16055d3c9736596a498de63c58
josselinonduty <contact@josselinonduty.fr>
2025-01-29 18:01:17 +0100

    fix: disable ssr (unused) to prevent hybrid rendering issues

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,6 +4,8 @@ export default defineNuxtConfig({
   devtools: { enabled: true },
   modules: ["@vueuse/nuxt", "@nuxt/ui"],
 
+  ssr: false,
+
   css: ["~/assets/css/main.css"],
   runtimeConfig: {
     public: {
