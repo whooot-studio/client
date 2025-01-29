cb9f3e370010c122c19159ee71f1e64127afdd9c
josselinonduty <contact@josselinonduty.fr>
2025-01-29 18:01:59 +0100

    fix: runtime config cannot be accessed from utils

---
+++ composables/api.ts
@@ -0,0 +1,21 @@
+export default function useApi() {
+  const api = useRuntimeConfig().public.api;
+  const baseUrl = `${api.host}:${api.port}/api`;
+
+  function createHttpEndpoint(path: string) {
+    return `${api.secure ? "https" : "http"}://${baseUrl}${path}`;
+  }
+
+  function createWsEndpoint(path: string) {
+    return `${api.secure ? "wss" : "ws"}://${baseUrl}${path}`;
+  }
+
+  const endpoints = {
+    quiz: createHttpEndpoint("/quiz"),
+    rooms: createWsEndpoint("/rooms"),
+  };
+
+  return {
+    endpoints,
+  };
+}
