c7e1b27600bceaca65204d5b7c838fe1d22317df
josselinonduty <contact@josselinonduty.fr>
2025-02-03 12:04:53 +0100

    feat: add discord blurple gradient

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,24 @@
 import type { Config } from "tailwindcss";
 import "tailwindcss/defaultTheme";
 
-export default <Partial<Config>>{};
+export default <Partial<Config>>{
+  theme: {
+    extend: {
+      colors: {
+        blurple: {
+          50: "#ECEEFE",
+          100: "#DEE0FC",
+          200: "#BDC2FA",
+          300: "#9CA3F7",
+          400: "#7A85F5",
+          500: "#5865F2",
+          600: "#1C2DED",
+          700: "#0E1DB9",
+          800: "#0A137B",
+          900: "#050A3E",
+          950: "#02041C",
+        },
+      },
+    },
+  },
+};
