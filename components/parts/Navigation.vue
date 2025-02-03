3c04b3e91a409582986be21cb31513e79e9bc1a2
josselinonduty <contact@josselinonduty.fr>
2025-02-03 12:06:20 +0100

    feat: create navigation bar component

---
+++ components/parts/Navigation.vue
@@ -0,0 +1,58 @@
+<script setup lang="ts">
+import authClient from "~/lib/auth";
+
+const { useSession } = authClient;
+const session = useSession();
+
+const links = computed(() => {
+  const _links = [
+    [
+      {
+        label: "Home",
+        icon: "tabler:home",
+        to: "/",
+      },
+      {
+        label: "Quiz",
+        icon: "tabler:list",
+        to: "/quiz",
+        disabled: !session.value.data?.user,
+      },
+    ],
+    [
+      !session.value.data?.user && {
+        label: "Register",
+        icon: "tabler:user-plus",
+        to: "/auth/register",
+      },
+      !session.value.data?.user && {
+        label: "Login",
+        icon: "tabler:user",
+        to: "/auth/login",
+      },
+      session.value.data?.user && {
+        label: "Profile",
+        to: "/settings/profile",
+        avatar: {
+          src: session.value.data.user.image as string,
+          alt: session.value.data.user.name,
+        },
+      },
+      session.value.data?.user && {
+        label: "Logout",
+        icon: "tabler:logout",
+        to: "/auth/logout",
+      },
+    ],
+  ];
+
+  return _links.map((list) => list.filter(Boolean)) as Exclude<
+    (typeof _links)[number][number],
+    undefined | false
+  >[][];
+});
+</script>
+
+<template>
+  <UHorizontalNavigation :links="links" />
+</template>
