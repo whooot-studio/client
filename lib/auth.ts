5b96a8ae4a1c73e200b2a37e7e0f8a673b3a3173
josselinonduty <contact@josselinonduty.fr>
2025-02-03 12:05:10 +0100

    feat: support generic oauth client for simplelogin

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,9 +1,11 @@
+import { genericOAuthClient } from "better-auth/client/plugins";
 import { createAuthClient } from "better-auth/vue";
 import useApi from "~/composables/api";
 
 const api = useApi().endpoints;
 const client = createAuthClient({
   baseURL: api.auth,
+  plugins: [genericOAuthClient()],
 });
 type client = typeof client;
 
