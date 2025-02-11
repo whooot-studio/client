e95b4b25fec55e81c82a3836a9c64e79c47c0ed2
josselinonduty <contact@josselinonduty.fr>
2025-02-11 19:02:37 +0100

    fix: use dynamic secure configuration for HTTP and WebSocket endpoints

--- composables/api.ts
+++ composables/api.ts
@@ -1,16 +1,18 @@
 export default function useApi() {
   const api = useRuntimeConfig().public.api;
+
   const host = import.meta.server ? api.ssrHost : api.host;
   const port = import.meta.server ? api.ssrPort : api.port;
   const secure = import.meta.server ? api.ssrSecure : api.secure;
+
   const baseUrl = `${host}${port ? ":" + port : ""}/api`;
 
   function createHttpEndpoint(path: string) {
-    return `${api.secure ? "https" : "http"}://${baseUrl}${path}`;
+    return `${secure ? "https" : "http"}://${baseUrl}${path}`;
   }
 
   function createWsEndpoint(path: string) {
-    return `${api.secure ? "wss" : "ws"}://${baseUrl}${path}`;
+    return `${secure ? "wss" : "ws"}://${baseUrl}${path}`;
   }
 
   const endpoints = {
