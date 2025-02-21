80bd6c5bba9bc00254447b3430da301e27789b06
josselinonduty <contact@josselinonduty.fr>
2025-02-22 00:17:29 +0900

    feat: add PlayerAvatar component for displaying player information in game

---
+++ components/quiz/PlayerAvatar.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+defineProps<
+  Partial<{
+    player: {
+      id: string;
+      name: string;
+      image?: string;
+    };
+  }>
+>();
+</script>
+<template>
+  <UAvatar v-if="player" size="xl" :src="player.image" :alt="player.name" />
+  <UAvatar v-else size="xl" alt="user" icon="tabler:user" />
+</template>
