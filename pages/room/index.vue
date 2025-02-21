e9f611571d654f9318833bc6a69924194b23579c
josselinonduty <contact@josselinonduty.fr>
2025-02-22 00:18:27 +0900

    feat: add a simple page to join a room using a room code

---
+++ pages/room/index.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+const code = ref<string>("");
+
+const router = useRouter();
+const join = () => {
+  if (!code.value) return;
+
+  router.push(`/room/${code.value}`);
+};
+</script>
+
+<template>
+  <UContainer :ui="{ constrained: 'max-w-3xl space-y-2' }">
+    <p class="text-2xl">Join a room</p>
+
+    <div class="flex gap-4">
+      <UInput
+        type="text"
+        size="lg"
+        v-model="code"
+        placeholder="Enter room code"
+      />
+      <UButton @click="join" size="lg">Join</UButton>
+    </div>
+  </UContainer>
+</template>
