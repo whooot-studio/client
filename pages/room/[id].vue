c8d5e76b6536470559632f52fd784f3f5b37d774
josselinonduty <contact@josselinonduty.fr>
2025-02-22 14:16:48 +0900

    feat: add username selection form w/ validation

--- pages/room/[id].vue
+++ pages/room/[id].vue
@@ -1,5 +1,8 @@
 <script setup lang="ts">
+import type { FormSubmitEvent } from "#ui/types";
+import PlayerAvatar from "~/components/quiz/PlayerAvatar.vue";
 import useApi from "~/composables/api";
+import { UsernameSchema } from "~/schema/game.schema";
 
 type User = {
   id: string;
@@ -7,30 +10,22 @@ type User = {
 };
 
 const route = useRoute();
-const members = ref<Map<string, User>>(new Map());
+const toast = useToast();
 
-const game = ref<{
+const game = reactive<{
   state: "idle" | "started" | "ended";
-  question?: string;
-  options?: string[];
+  members: Map<string, User>;
 }>({
   state: "idle",
+  members: new Map(),
+});
+
+const usernameState = reactive({
+  username: "Anonymous",
 });
 
 const { endpoints } = useApi();
 const { send } = useWebSocket(endpoints.rooms, {
-  onConnected: async (ws) => {
-    if (!route.params.id) return;
-
-    console.log("Joining room", route.params.id);
-    ws.send(
-      JSON.stringify({
-        action: "meta:join",
-        code: route.params.id,
-      })
-    );
-  },
-
   onMessage: async (socket, event) => {
     const blob: Blob = event.data;
     if (!blob) return;
@@ -42,46 +37,111 @@ const { send } = useWebSocket(endpoints.rooms, {
 
     switch (payload.action) {
       case "members:join":
-        members.value.set(payload.member.id, payload.member);
+        game.members.set(payload.member.id, payload.member);
         break;
 
       case "members:leave":
-        members.value.delete(payload.member.id);
+        game.members.delete(payload.member.id);
         break;
 
       case "members:all":
         for (const [id, member] of Object.entries(payload.members)) {
-          members.value.set(id, member as User);
+          game.members.set(id, member as User);
         }
         break;
 
       case "game:start":
-        game.value.state = "started";
+        game.state = "started";
         break;
 
-      case "game:question:start":
-        game.value.question = payload.question;
-        game.value.options = payload.options;
+      case "meta:error":
+        {
+          const { code, message } = payload;
+          toast.add({
+            title: code || "Error",
+            description: message || "An error occured",
+            icon: "tabler:alert-circle",
+            timeout: 8000,
+            color: "red",
+          });
+        }
         break;
 
-      case "game:question:end":
-        game.value.question = undefined;
-        game.value.options = undefined;
-        break;
+      // case "game:question:start":
+      //   game.question = payload.question;
+      //   game.options = payload.options;
+      //   break;
+
+      // case "game:question:end":
+      //   game.question = undefined;
+      //   game.options = undefined;
+      //   break;
     }
   },
 });
+
+function defineUsername(event: FormSubmitEvent<UsernameSchema>) {
+  return send(
+    JSON.stringify({
+      action: "meta:join",
+      code: route.params.id,
+      name: event.data.username,
+    })
+  );
+}
 </script>
 
 <template>
-  <p v-if="members" v-for="[id, member] in members" :key="id">
-    {{ member.name }}
-  </p>
+  <UContainer :ui="{ constrained: 'max-w-3xl' }">
+    <section v-if="game.state === 'idle'" class="space-y-4">
+      <UForm
+        :state="usernameState"
+        :schema="UsernameSchema"
+        @submit="defineUsername"
+        class="max-w-md mx-auto space-y-2"
+      >
+        <UFormGroup name="username" label="Username" required>
+          <UInput
+            type="string"
+            icon="tabler:user"
+            v-model="usernameState.username"
+          />
+        </UFormGroup>
 
-  <template v-if="game.state === 'started'">
-    <p>{{ game.question }}</p>
-    <p v-for="option in game.options" :key="option">
-      {{ option }}
-    </p>
-  </template>
+        <UButton type="submit" block>
+          Join
+          <template v-if="usernameState.username">
+            as
+            <span class="underline underline-offset-4">{{
+              usernameState.username
+            }}</span>
+          </template>
+        </UButton>
+      </UForm>
+
+      <UDivider />
+
+      <UAvatarGroup
+        v-if="game.members.size > 0"
+        :ui="{ wrapper: 'flex-row justify-start flex-wrap' }"
+      >
+        <PlayerAvatar
+          v-for="[id, member] in game.members"
+          :key="id"
+          :player="member"
+        />
+      </UAvatarGroup>
+      <template v-else>
+        <p class="text-sm italic text-gray-700">
+          Waiting for players to join...
+        </p>
+
+        <UAvatarGroup class="animate-pulse">
+          <PlayerAvatar />
+          <PlayerAvatar />
+          <PlayerAvatar />
+        </UAvatarGroup>
+      </template>
+    </section>
+  </UContainer>
 </template>
